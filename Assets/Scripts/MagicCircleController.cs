aa326689fa742d85f04acbf846e685c746d33bb6
mentor-oonuki <mentor.oonuki@gmail.com>
2017-02-13 11:03:47 +0900

    【追加】 MagicCircle を 追加

---
+++ Assets/Scripts/MagicCircleController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicCircleController : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject Target;
+
+    void Start () {
+        		
+	}
+
+	void Update () {
+        transform.LookAt(Target.transform);
+        float Angle = Time.deltaTime * 20;
+        transform.Rotate(0.0f,0.0f, Angle);
+    }
+}
