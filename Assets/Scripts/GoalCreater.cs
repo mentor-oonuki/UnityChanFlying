83ea15aa67801bc95948b500da25dd5783f10124
mentor-oonuki <mentor.oonuki@gmail.com>
2017-02-17 05:03:02 +0900

    【追加】 ゴールをランダムに配置

---
+++ Assets/Scripts/GoalCreater.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalCreater : MonoBehaviour {
+
+    [SerializeField]
+    public GameObject GoalPrefab;
+
+
+	// Use this for initialization
+	void Start () {
+        int x = Random.Range(-930, -200);
+        int z = Random.Range(-660, -100);
+
+        GameObject.Instantiate(GoalPrefab, new Vector3(x, 200.0f, z), Quaternion.identity);
+	}
+	
+}
